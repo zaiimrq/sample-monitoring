b0685266c5cabd987c80490b11cedd682827d653
caramelmelt <ajengkalista08@gmail.com>
2024-03-23 16:13:25 +0900

    test 3

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sampel </title>
+</head>
+<body>
+    <h1>Sampel Projek 2</h1>
+</body>
+</html>
