28edaed3d71fa08d0d802ef227fd8932d85e1101
caramelmelt <ajengkalista08@gmail.com>
2024-03-27 21:19:17 +0900

    Buat Skema tb timses, operasional, dan permintaan masyarakat

---
+++ app/Http/Controllers/TimsesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\timses;
+use App\Http\Requests\StoretimsesRequest;
+use App\Http\Requests\UpdatetimsesRequest;
+
+class TimsesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoretimsesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(timses $timses)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(timses $timses)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatetimsesRequest $request, timses $timses)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(timses $timses)
+    {
+        //
+    }
+}
