40bf75f6de4747353ae54fd9891f84080f528031
zaiimrq <zaiimrq@gmail.com>
2024-03-30 14:11:11 +0900

    fix: wrong file in app

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Service\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\AuthenticationException;
+
+class LoginController extends Controller
+{
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request, UserService $userService)
+    {
+
+        $validated = $request->validate([
+            'username' => ['required', 'min:5', 'max:20'],
+            'password' => ['required', 'min:5']
+        ]);
+
+        try {
+            if ($userService->login($validated)) 
+            {
+                $user = User::findOrFail(Auth::user()->id);
+                $request->session()->regenerate();
+
+                return response()->redirectToIntended(
+                        $user->when($user->hasRole('admin'), 
+                            fn() => route('admin.dashboard'), 
+                            fn() => route('timses.dashboard')
+                        )
+                );
+            }
+
+        } catch(AuthenticationException $e){
+            
+            return back()->withErrors([
+                'username' => $e->getMessage()
+            ])->onlyInput('username');
+        }
+    
+    }
+}
